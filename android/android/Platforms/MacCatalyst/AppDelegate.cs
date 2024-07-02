5d54057e646e88aa09520ac64fc4340e7966b0d5
sad <ads>
2024-07-02 14:35:12 +0900

    tekitou

---
+++ android/android/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace android
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
