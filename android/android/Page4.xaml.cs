533028c4d8f92a33313cb5b4ae8b67690d07aa47
akita <zeal23310029@fsgcl.jp>
2024-07-12 15:09:20 +0900

    page4

--- android/android/Page4.xaml.cs
+++ android/android/Page4.xaml.cs
@@ -11,4 +11,9 @@ public partial class Page4 : ContentPage
         // サブページへ移動
         await Shell.Current.GoToAsync("//Page5");
     }
+    private async void OnClicked2(object sender, EventArgs e)
+    {
+        // サブページへ移動
+        await Shell.Current.GoToAsync("//Page3");
+    }
 }
