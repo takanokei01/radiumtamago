76a8f1fa3ea6cf5b6b7591d4c07d167bf76dde3f
takanokei01 <zeal23410219@fsgcl.jp>
2024-07-17 14:51:42 +0900

    page9desu

--- android/android/Page9.xaml.cs
+++ android/android/Page9.xaml.cs
@@ -11,5 +11,10 @@ public partial class Page9 : ContentPage
         // サブページへ移動
         await Shell.Current.GoToAsync("//Page10");
     }
-    
+    private async void OnClicked2(object sender, EventArgs e)
+    {
+        // サブページへ移動
+        await Shell.Current.GoToAsync("//Page8");
+    }
+
 }
