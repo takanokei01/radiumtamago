946a3a1f83a135d8793b344f5782ba39b95db7f5
akita <zeal23310029@fsgcl.jp>
2024-07-17 14:21:58 +0900

    page7 page11

--- android/android/Page11.xaml.cs
+++ android/android/Page11.xaml.cs
@@ -1,15 +1,15 @@
 namespace android;
 
-public partial class Page10 : ContentPage
+public partial class Page11 : ContentPage
 {
-	public Page10()
+	public Page11()
 	{
 		InitializeComponent();
 	}
     private async void OnClicked(object sender, EventArgs e)
     {
         // サブページへ移動
-        await Shell.Current.GoToAsync("//Page11");
+        await Shell.Current.GoToAsync("//MainPage");
     }
     private async void OnClicked2(object sender, EventArgs e)
     {
